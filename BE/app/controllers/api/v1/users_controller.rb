e1ff9a4206325fe85dc89649797663509a3a6486
phucphvtc <phucphvtc@gmail.com>
2022-09-23 18:00:47 +0700

    add validates

--- BE/app/controllers/api/v1/users_controller.rb
+++ BE/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,3 @@
-
-
 module Api
   module V1
     class UsersController < ApplicationController
@@ -7,24 +5,23 @@ module Api
         @user = User.all
         render json: @user
       end
-    
+
       def create
         @user = User.create(user_params)
-    
+
         if @user.valid?
-            token = encode_token({ user_id: @user.id })
-            render json: { user: @user, token: token }, status: :ok
+          token = encode_token({ user_id: @user.id })
+          render json: { user: @user, token: token }, status: :ok
         else
-            render json: { error: ' invalid name or password', user: @user}, status: :unprocessable_entity 
+          render json:  @user.errors.full_messages , status: :unprocessable_entity
         end
       end
+
       private
+
       def user_params
         params.permit(:password, :email)
       end
-    
-
     end
   end
-
 end
