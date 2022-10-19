ce624b3f904e7110b5f13b90ebcd2e991dd7141d
phucphvtc <phucphvtc@gmail.com>
2022-10-19 09:07:03 +0700

    add column name to comments

--- BE/db/schema.rb
+++ BE/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_044313) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_020422) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_044313) do
     t.bigint "reply_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["pr_attribute_id"], name: "index_comments_on_pr_attribute_id"
   end
 
