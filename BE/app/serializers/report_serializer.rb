680eaf36d6036fa321919e07f775ee83b9cf7801
phucphvtc <phucphvtc@gmail.com>
2022-10-26 11:22:36 +0700

    add date_time report

--- BE/app/serializers/report_serializer.rb
+++ BE/app/serializers/report_serializer.rb
@@ -1,7 +1,11 @@
 class ReportSerializer < ActiveModel::Serializer
-  attributes :id, :comment_id, :content_comment,:reason, :status, :created_at, :updated_at
+  attributes :id, :comment_id, :content_comment,:reason, :status, :date_time
 
   def content_comment
     object.comment
+  end
+
+  def date_time
+    object.created_at.to_datetime.strftime('%a %d %b  %Y')
   end
 end
